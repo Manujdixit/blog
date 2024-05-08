5a4fba8c2c1a7d1aa84e39fbef16b2d600a3364f
Manujdixit <manujdixit04@gmail.com>
2024-05-09 00:06:54 +0530

    config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,1 @@
-// export const BACKEND_URL = "https://backend.manujdixit04.workers.dev";
-
-// export const BACKEND_URL = "http://localhost:8787";
-
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
